9e68c48df57e063810124acaa607dd41b5b5634d
HolaWanli <125253786+HolaWanli@users.noreply.github.com>
2023-04-28 02:27:03 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "wanli",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "",
+    bio: "简单，一切因此而简单",
+    email: "wanli@leshgo.ml",
+    linkedin: "",
+    github: "",
     instagram: "",
   },
   projects: [
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Wanli_log",
+    description: "welcome to my log!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
